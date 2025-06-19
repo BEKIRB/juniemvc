f631efc4907bf19586d2659dbecbda6c34675ac7
BEKIRB <bekirbalcik@gmail.com>
2025-06-19 14:32:29 +0100

    feat(junie): added .devcontainer.json

--- src/main/java/guru/springframework/juniemvc/entities/Beer.java
+++ src/main/java/guru/springframework/juniemvc/entities/Beer.java
@@ -11,6 +11,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -36,9 +38,11 @@ public class Beer {
     private Integer quantityOnHand;
     private BigDecimal price;
 
+    @CreationTimestamp
     @Column(updatable = false)
     private LocalDateTime createdDate;
 
+    @UpdateTimestamp
     @Column
     private LocalDateTime updateDate;
 }
